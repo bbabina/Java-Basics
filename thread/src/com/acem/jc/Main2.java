e638356f6c440e8db34d0e5a8e5bd9852516e884
Babina Banjara <offers682@gmail.com>
2022-07-09 09:49:54 +0545

    multithreading

---
+++ thread/src/com/acem/jc/Main2.java
@@ -0,0 +1,15 @@
+public class Main2 {
+
+    public static void main(String args[]) {
+        Player player = new Player(10);
+        System.out.println(Thread.currentThread().getName() + " started " + player.getName());
+        player.start();
+        try {
+            player.join();
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+        System.out.println(Thread.currentThread().getName() + " waited for " + player.getName());
+    }
+
+}
